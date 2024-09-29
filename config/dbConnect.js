5a4ca124134df7227df167c4be69c5ee8766eaa6
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-09-29 10:49:24 +0530

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 export const connectDb = async () => {
-  console.log(process.env.DB_LOCAL_URI);
+  
   await mongoose
     .connect(process.env.DB_LOCAL_URI)
     .then((con) =>
